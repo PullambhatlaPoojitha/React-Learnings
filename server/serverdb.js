8ff0d0c938a8d3a786e8a9145d19f17bb46371d0
20B01A12E6 IT <20b01a12e6@svecw.edu.in>
2022-06-15 00:36:49 +0530

    first commit

---
+++ server/serverdb.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const mysql = require('mysql');
+const db = mysql.createPool({
+    connectionLimit: 10,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "ev_stations",
+
+});
+app = express();
+var cors = require('cors');
+app.use(cors());
+const bodyParser = require('body-parser');
+
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+// db.connect((err) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log("connected.");
+//     }
+// });
+
+app.get("/", (req, res) => {
+    console.log("your server has been started..");
+})
+app.post("/insert", (req, res) => {
+    const EV_ID = req.body.EV_ID;
+    const EV_NAME = req.body.EV_NAME;
+    const EV_PLACE = req.body.EV_PLACE;
+    const insert = "insert into evstat_india (EV_ID, EV_NAME, EV_PLACE) values(?, ?, ?)";
+    db.query(insert, [EV_ID, EV_NAME, EV_PLACE], (err, data) => {
+        console.log(err);
+    })
+})
+app.post("/delete/:EV_ID",(req,res)=>{
+    const EV_ID=req.params.EV_ID;
+    const del="delete from evstat_india where evstat_india.EV_ID=?";
+    db.query(del,[EV_ID],(err,data)=>{
+        console.log(err);
+    })
+})
+app.get("/rep", (req, res) => {
+    console.log("Please Wait! we are fetching your data...");
+    db.query("select * from evstat_india", (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log(data);
+            res.send(data);
+        }
+    });
+});
+app.listen(3000, () => {
+    console.log("ushh! your server is listening...");
+})
